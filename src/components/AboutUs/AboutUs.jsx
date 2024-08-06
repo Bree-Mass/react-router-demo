d7fad98a1d2d5834bea82532f79caeb23ab5d847
Bree Massingill <breemassingill@gmail.com>
2024-08-06 12:54:00 -0500

    add routes

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,9 +1,19 @@
+import { Link, Outlet } from "react-router-dom";
 import "./AboutUs.css";
 
 function AboutUs() {
   return (
     <div className="about-us">
+      <ul>
+        <li>
+          <Link to="site-mission">Site Misson</Link>
+        </li>
+        <li>
+          <Link to="site-history">Site History</Link>
+        </li>
+      </ul>
       <p>You can find out more information about our site here.</p>
+      <Outlet />
     </div>
   );
 }
